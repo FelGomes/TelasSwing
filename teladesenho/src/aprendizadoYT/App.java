b35f8a485444d64714b1d247aa8e2692abfeacc9
Felipe Gomes <felipeferreiraag0@gmail.com>
2025-06-07 23:04:37 -0300

    apenas o basico

---
+++ teladesenho/src/aprendizadoYT/App.java
@@ -0,0 +1,57 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package aprendizadoYT;
+
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JTextField;
+
+/**
+ *
+ * @author felipe
+ */
+public class App {
+    
+    public static void main(String[] args) {
+        JFrame janela = new JFrame();
+        
+        // definir o tamanho da janela
+        janela.setBounds(400,600,400,600);
+        janela.setLayout(null);
+        
+        //Button -> BOTAO
+        JButton botao = new JButton("Clique AQUI!");
+        botao.setBounds(10, 10, 200, 30);
+        
+        // texto
+        JLabel LabelUsuario = new JLabel("Usuarios");
+        LabelUsuario.setBounds(50,50,100,30);
+        
+        JTextField campoUsuario = new JTextField();
+        campoUsuario.setBounds(50,90, 100, 30 );
+        
+        
+        botao.addActionListener(new ActionListener(){
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                System.out.println("Aprendendo a fazer uma tela!!");
+                
+            }
+            
+        });
+        
+        
+        
+        janela.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); //constante (encerrar ao fechar)
+        janela.setVisible(true);
+        janela.add(botao);
+        janela.add(LabelUsuario);
+        janela.add(campoUsuario);
+    }
+    
+}
